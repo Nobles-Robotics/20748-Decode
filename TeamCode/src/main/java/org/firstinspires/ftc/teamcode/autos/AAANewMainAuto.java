20010bc576d8badc745e2253e95d4a590331f254
Jeffrey <boby8k@gmail.com>
2026-02-21 11:07:07 -0500

    new auto to protect from errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AAANewMainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AAANewMainAuto.java
@@ -27,6 +27,7 @@ import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.CommandManager;
 import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.delays.WaitUntil;
+import dev.nextftc.core.commands.groups.ParallelDeadlineGroup;
 import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.components.BindingsComponent;
@@ -66,8 +67,8 @@ public class AAANewMainAuto extends NextFTCOpMode {
     public static final Pose intakeAlign1Blue = new Pose(45, 84, Math.toRadians(180));
     public static final Pose intake1Blue = new Pose(12, 84, Math.toRadians(180));
 
-    public static final Pose intakeAlign3Blue = new Pose(45, 36, Math.toRadians(180));
-    public static final Pose intake3Blue = new Pose(6, 36, Math.toRadians(180));
+    public static final Pose intakeAlign3Blue = new Pose(45, 34, Math.toRadians(180));
+    public static final Pose intake3Blue = new Pose(9, 34, Math.toRadians(180));
 
     public static final Pose targetExitPosBlue = new Pose(25, 50, Math.toRadians(315));
 
@@ -110,21 +111,22 @@ public class AAANewMainAuto extends NextFTCOpMode {
                 new FollowPath(intakeAlign1Path),
                 new InstantCommand(Intake.on()),
                 new Delay(standardDelay),
-                new ParallelGroup(
-                        new SequentialGroupFixed(
-                                new InstantCommand(Intake.on()),
-                                new FollowPath(intake1Path, true, 0.5),
-                                new Delay (0.05)
-                        ),
-                        new SequentialGroupFixed(
-                                Robot.intakeAll
+                new ParallelDeadlineGroup(
+                        new Delay(2),
+                        new ParallelGroup(
+                            new SequentialGroupFixed(
+                                    new InstantCommand(Intake.on()),
+                                    new FollowPath(intake1Path, true, 0.5),
+                                    new Delay (0.05)
+                            ),
+                            Robot.intakeAll
                         )
                 ),
                 new Delay(standardDelay),
                 new InstantCommand(Outtake.on),
                 new InstantCommand(Storage.spinToNextOuttakeIndex()),
-                new FollowPath(score1Path),
                 new InstantCommand(Intake.reverse()),
+                new FollowPath(score1Path),
                 new WaitUntil(() -> !follower().isBusy()),
                 new InstantCommand(Intake.off()),
                 Robot.outtakeAllSmooth,
@@ -133,22 +135,22 @@ public class AAANewMainAuto extends NextFTCOpMode {
                 new FollowPath(intakeAlign3Path),
                 new InstantCommand(Intake.on()),
                 new Delay(standardDelay),
-                new ParallelGroup(
-                        new SequentialGroupFixed(
-                                new InstantCommand(Intake.off()),
-                                new FollowPath(intake3Path, true, 0.7),
-                                new Delay (0.05)
-                        ),
-                        new SequentialGroupFixed(
+                new ParallelDeadlineGroup(
+                        new Delay(2),
+                        new ParallelGroup(
+                                new SequentialGroupFixed(
+                                        new InstantCommand(Intake.on()),
+                                        new FollowPath(intake3Path, true, 0.7),
+                                        new Delay (0.05)
+                                ),
                                 Robot.intakeAll
                         )
-
                 ),
                 new Delay(standardDelay),
                 new InstantCommand(Outtake.on),
                 new InstantCommand(Storage.spinToNextOuttakeIndex()),
-                new FollowPath(score3Path),
                 new InstantCommand(Intake.reverse()),
+                new FollowPath(score3Path),
                 new WaitUntil(() -> !follower().isBusy()),
                 new InstantCommand(Intake.off()),
                 new Delay(standardDelay),
@@ -277,6 +279,7 @@ public class AAANewMainAuto extends NextFTCOpMode {
 
 
     public void onStop(){
+        CommandManager.INSTANCE.cancelAll();
         endPose = follower().getPose();
         autoendPose = follower().getPose();
     }
